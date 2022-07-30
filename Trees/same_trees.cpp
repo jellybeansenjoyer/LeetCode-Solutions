32d5b60aa416679a48e89b3e101c8a7b0fbf2f1d
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-07-30 16:56:26 +0530

    is same tree

---
+++ Trees/same_trees.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL&&q==NULL){
+            return true;
+        }
+        if(p!=NULL&&q!=NULL){
+            if(p->val!=q->val){
+                return false;
+            }else{
+                return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+            }
+        }else
+            return false;
+}
